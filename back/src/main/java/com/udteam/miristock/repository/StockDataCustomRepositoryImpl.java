6036bedf1dc642e37095bcc22e16b9feaff573e3
White-Asher <dkxmpower999@naver.com>
2023-01-31 23:31:23 +0900

    [Feat] QueryDSL 환경셋팅, 메인페이지 등락률 Top5 종목 출력, 단일종목 상세 출력
    QuseyDSl gradle 초기환경 셋팅, 메인페이지에 해당날짜 등락률이 높은 종목, 낮은 종목 출력, 단일 종목 페이지 이동 시 날짜범위에 따라 주식데이터 출력
    
    Issue : S08P12B111-420, S08P12B111-421, S08P12B111-422, S08P12B111-423

---
+++ back/src/main/java/com/udteam/miristock/repository/StockDataCustomRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.udteam.miristock.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.udteam.miristock.entity.StockDataEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.udteam.miristock.entity.QStockDataEntity.stockDataEntity;
+
+
+@RequiredArgsConstructor
+@Repository
+public class StockDataCustomRepositoryImpl implements StockDataCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    private BooleanExpression isSearchEndDateNull(Integer searchStartDate, Integer searchEndDate){
+        if(searchEndDate == null) {
+            return stockDataEntity.stockDataDate.eq(searchStartDate);
+        } else {
+            return stockDataEntity.stockDataDate.between(searchStartDate, searchEndDate);
+        }
+    }
+
+    @Override
+    public List<StockDataEntity> findStockData(Integer searchStartDate, Integer searchEndDate, String searchStockCode) {
+
+        JPAQuery<StockDataEntity> query = jpaQueryFactory
+                    .selectFrom(stockDataEntity)
+                    .where(stockDataEntity.stockCode.eq(searchStockCode)
+                            .and(isSearchEndDateNull(searchStartDate, searchEndDate))
+                    ).fetchAll();
+        List<StockDataEntity> result = query.fetch();
+        return result;
+
+    }
+}
