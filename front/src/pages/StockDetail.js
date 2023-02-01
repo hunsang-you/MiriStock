bad7226798655c5d46cbfc93eae1aaa1580a849f
dostiny <dogyeom2@gmail.com>
2023-02-01 22:47:56 +0900

    [Feat] 차트 수정
    차트 라이브러리 교체 후 추가
    
    Issue : S08P12B111-380

--- front/src/pages/StockDetail.js
+++ front/src/pages/StockDetail.js
@@ -1,57 +1,81 @@
-import './Charts.css';
+import './css/Charts.css';
 import { useState, useEffect } from 'react';
-import LineChart from './LineChart'
-import BarChart from './BarChart'
-import Button from '@mui/material/Button';
+
+import TestChart from '../components/chart/TestChart';
+import { Button } from '@mui/material';
 
 const StockDetail = () => {
+  const [stockData, setStockData] = useState([]);
+  const [series, setSeries] = useState([
+    {
+      name: '오늘의 주식 종가',
+      data: stockData,
+    },
+  ]);
 
-    const [stockData, setStockData] = useState([]);
-    const [series, setSeries] = useState(
-        [{
-        name: " 오늘의 주식 종가",
+  useEffect(() => {
+    //   console.log("들어가버려");
+    setSeries([
+      {
+        name: '오늘의 주식 종가',
         data: stockData,
-        },]
-    )
+      },
+    ]);
+  }, [stockData]);
 
-    useEffect(() => {
-        console.log(22);
-        setSeries([{
-        name: " 오늘의 주식 종가",
-        data: stockData,
-        },]);
-    }, [stockData]);
+  const inserData = () => {
+    if (stockData.length === 50) {
+      let copy = [...stockData];
+      copy.shift();
+      copy.push(Math.random() * 100);
+      setStockData(copy);
+    } else {
+      setStockData([...stockData, Math.random() * 100]);
+    }
+  };
 
-
-    const inserData = () => {
-        // console.log(stockData)
-        // console.log(stockData.length)
-        if (stockData.length === 50) {
-        let copy = [...stockData]
-        copy.shift();
-        copy.push(Math.random() * 100);
-        setStockData(copy);
-        } else {
-        setStockData([...stockData, Math.random() * 100]);
-        }
-    };
-
-    return (
-        <div>
-            <h1>Detail</h1>
-            
-            <Button onClick={ () => {inserData()} } variant="contained" color="primary">제발~~</Button>
-            <LineChart series={series} />
-            <BarChart series={series} />
-            <div className='space-between'>
-                <Button variant="contained" color="primary">매수</Button>
-                <Button variant="contained" color="primary">매도</Button>
-            </div>
-            <div>
-                ddd
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Detail</h1>
+      <div>
+        <TestChart />
+      </div>
+      <div className="space-between space-margin">
+        <Button
+          style={{ width: '49%' }}
+          variant="contained"
+          color="primary"
+          disableElevation
+        >
+          매수
+        </Button>
+        <Button
+          style={{ width: '49%' }}
+          variant="contained"
+          color="primary"
+          disableElevation
+        >
+          매도
+        </Button>
+      </div>
+      <div className="space-margin divbox">
+        <h3>주요 뉴스</h3>
+        <div>낄낄 뉴스내용</div>
+        <div>낄낄 뉴스내용</div>
+        <div>낄낄 뉴스내용</div>
+        <div>낄낄 뉴스내용</div>
+      </div>
+      <div className="space-margin divbox">
+        <h3>재무 제표</h3>
+        <div>낄낄 또 차트 들어가야됨</div>
+        <div>낄낄 또 차트 들어가야됨</div>
+        <div>낄낄 또 차트 들어가야됨</div>
+        <div>낄낄 또 차트 들어가야됨</div>
+        <div>낄낄 또 차트 들어가야됨</div>
+        <div>낄낄 또 차트 들어가야됨</div>
+      </div>
+    </div>
+  );
+};
 
 export default StockDetail;
