de60030e5700ea27f7d0064795ee059113eab159
White-Asher <dkxmpower999@naver.com>
2023-02-02 16:18:09 +0900

    [Feat] 당일 거래량 가장 많은 주식 종목 5개 출력 구현
    메인페이지에 당일 거래량이 가장 많은 주식 종목 5개 출력 구현
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/service/StockDataService.java
+++ back/src/main/java/com/udteam/miristock/service/StockDataService.java
@@ -20,22 +20,25 @@ public class StockDataService {
     private final StockDataRepository stockDataRepository;
     private final StockDataCustomRepository stockDataCustomRepository;
 
+    // 거래량 가장 많은 5개 출력
+    public List<StockDataResponseDto> findTop5AmountDesc(Integer stockDataDate) {
+        return stockDataRepository.findTopByStockCodeOrderByStockDataAmountDesc(stockDataDate)
+        .stream().map(StockDataResponseDto::new)
+        .collect(Collectors.toList());
+    }
+
     // 등락률 가장 낮은 5개 순서대로 가져오기
     public List<StockDataResponseDto> findTop5BySFlucauationRateAsc(Integer stockDataDate){
-        List<StockDataResponseDto> stockDataEntityList
-                = stockDataRepository.findTop5ByStockDataDateOrderByStockDataFlucauationRateAsc(stockDataDate)
-                .stream().map(StockDataResponseDto::new)
-                .collect(Collectors.toList());
-        return stockDataEntityList;
+        return stockDataRepository.findTop5ByStockDataDateOrderByStockDataFlucauationRateAsc(stockDataDate)
+        .stream().map(StockDataResponseDto::new)
+        .collect(Collectors.toList());
     }
 
     // 등락률 가장 높은 순서대로 5개 출력
     public List<StockDataResponseDto> findTop5BySFlucauationRateDesc(Integer stockDataDate){
-        List<StockDataResponseDto> stockDataEntityList
-                = stockDataRepository.findTop5ByStockDataDateOrderByStockDataFlucauationRateDesc(stockDataDate)
-                .stream().map(StockDataResponseDto::new)
-                .collect(Collectors.toList());
-        return stockDataEntityList;
+        return stockDataRepository.findTop5ByStockDataDateOrderByStockDataFlucauationRateDesc(stockDataDate)
+        .stream().map(StockDataResponseDto::new)
+        .collect(Collectors.toList());
     }
 
     public List<StockDataResponseDto> findStockData(Integer searchStartDate, Integer searchEndDate, String searchStockCode){
@@ -43,6 +46,5 @@ public class StockDataService {
                 .stream().map(StockDataResponseDto::new)
                 .collect(Collectors.toList());
     }
-
 
 }
