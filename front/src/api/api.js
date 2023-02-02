03cf5f7596c169b9f78df867a116cc332395b9be
unknown <tkdgusdl63@naver.com>
2023-02-02 14:11:08 +0900

    [Feat] axios추가
    하단네비바 store저장 및 axios추가

---
+++ front/src/api/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://192.168.31.160';
+const token =
+  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0a2RndXNkbDYzQG5hdmVyLmNvbSIsInJvbGUiOiJNRU1CRVIiLCJleHAiOjE2NzUzMTY2MDZ9.yE2kwDnuQvR4B9ypwHElsJ9jngDk-aeIrf-rxwnMnQw';
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  },
+});
+
+export default api;
+
+export const memberAPI = {
+  test: () => api.get(`/login/oauth2/code/kakao`),
+};
+
+export const rankAPI = {
+  increase: () => api.get(`/stockdata/rate/increase/${20210112}`),
+  decrease: () => api.get(`/stockdata/rate/decrease/${20210112}`),
+  capital: () => api.get(`/stockdata/amount/top`),
+};
+
+export const stockAPI = {
+  stockDetail: (stockCode, startDate, endDate) =>
+    api.get(`stockdata/detail`, {
+      params: { stockCode: stockCode, startDate: startDate, endDate: endDate },
+    }),
+};
--- front/src/index.js
+++ front/src/index.js
@@ -9,13 +9,11 @@ import { theme } from './static/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
