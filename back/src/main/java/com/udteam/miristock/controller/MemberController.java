466f491766ba1e37fe909abcb8a077aaaa587fb8
White-Asher <dkxmpower999@naver.com>
2023-02-02 13:41:42 +0900

    [Refactor] MemberController 불필요한 매개변수 제거, Oauthsuccess핸들러 닉네임 부분 수정
    MemberController Auth 매개변수 제거, Oauthsuccesshandler nickname체크 부분 수정
    
    Issue : none

--- back/src/main/java/com/udteam/miristock/controller/MemberController.java
+++ back/src/main/java/com/udteam/miristock/controller/MemberController.java
@@ -35,11 +35,9 @@ public class MemberController {
 
     @DeleteMapping
     @ApiOperation(value = "해당 유저 회원 탈퇴 처리")
-    public ResponseEntity<String> deleteMember(@RequestHeader String Authorization, Authentication auth){
+    public ResponseEntity<String> deleteMember(@RequestHeader String Authorization){
         String userDetails = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String userDetails1 = (String) auth.getPrincipal();
         String token = HeaderUtil.getAccessTokenString(Authorization);
-        log.info("유저 디테일1 = {}",userDetails1);
         log.info("유저 디테일 = {}",userDetails);
         if (memberservice.deleteMember(token)!= 0){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error");
@@ -50,17 +48,13 @@ public class MemberController {
 
     @PutMapping("/nickname")
     @ApiOperation(value = " 유저 닉네임 설정")
-    public ResponseEntity<MemberDto> updateMember(@RequestHeader String Authorization,String nickname,Authentication auth){
+    public ResponseEntity<MemberDto> updateMember(@RequestHeader String Authorization, @RequestParam String nickname){
         String userDetails = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String userDetails1 =(String) auth.getPrincipal();
         String token = HeaderUtil.getAccessTokenString(Authorization);
-        log.info("유저 디테일1 = {}",userDetails1);
         log.info("유저 디테일 = {}",userDetails);
         if(memberservice.selectOneMember(token)== null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return ResponseEntity.ok()
-                .body(memberservice.updateMember(token,nickname));
+        return ResponseEntity.ok().body(memberservice.updateMember(token,nickname));
     }
 }
