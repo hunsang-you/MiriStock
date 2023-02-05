e82a7ea4ce58cecc14f2b3260874a93434ecfb93
hunsang-you <gjstkd123@naver.com>
2023-02-05 23:24:42 +0900

    [Feat] 더보기 작업

--- front/src/pages/More.js
+++ front/src/pages/More.js
@@ -1,8 +1,17 @@
+import MoreMain from '../components/more/MoreMain';
+import ChangeName from '../components/more/ChangeName';
+import GameExit from '../components/more/GameExit';
+import Portfolio from '../components/more/Portfolio';
+import { Routes, Route } from 'react-router-dom';
+
 const More = () => {
   return (
-    <div>
-      <p>더보기창입니다.</p>
-    </div>
+    <Routes>
+      <Route path="" element={<MoreMain />} />;
+      <Route path="/change" element={<ChangeName />} />;
+      <Route path="/exit" element={<GameExit />} />;
+      <Route path="/result" element={<Portfolio />} />;
+    </Routes>
   );
 };
 
