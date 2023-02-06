f7192001a79cd05b880fbdb5526acbc23d811955
White-Asher <dkxmpower999@naver.com>
2023-02-06 22:01:10 +0900

    [Feat] 회원 시뮬레이션 기능 날짜변경 기능 개발
    회원 시뮬레이션 기능중 날짜 추가 요청이 들어오면 stockdata table의 날짜가 있는지 확인 후 날짜 변경하는 스켈레톤 코드 작성함
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/repository/StockDataRepository.java
+++ back/src/main/java/com/udteam/miristock/repository/StockDataRepository.java
@@ -1,10 +1,9 @@
 package com.udteam.miristock.repository;
 
+import com.udteam.miristock.dto.StockDataInfoMapping;
 import com.udteam.miristock.entity.StockDataEntity;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,5 +20,6 @@ public interface StockDataRepository extends JpaRepository<StockDataEntity, Inte
     // 등락률 가장 높은 순서대로 5개 출력
     List<StockDataEntity> findTop5ByStockDataDateOrderByStockDataFlucauationRateDesc(Integer stockDataDate);
 
-
+    // 해당 날짜에 데이터가 있는지 확인
+    StockDataInfoMapping findTop1ByStockDataDate(Integer stockDataDate);
 }
--- back/src/main/java/com/udteam/miristock/service/SimulationService.java
+++ back/src/main/java/com/udteam/miristock/service/SimulationService.java
@@ -1,12 +1,16 @@
 package com.udteam.miristock.service;
 
 import com.udteam.miristock.dto.SimulEndDto;
+import com.udteam.miristock.dto.StockDataInfoMapping;
 import com.udteam.miristock.repository.MemberAssetRepository;
 import com.udteam.miristock.repository.MemberStockRepository;
+import com.udteam.miristock.repository.StockDataRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.udteam.miristock.service.InformationService.AddDate;
 
 @Slf4j
 @Service
@@ -15,6 +19,7 @@ public class SimulationService {
 
     private final MemberAssetRepository memberAssetRepository;
     private final MemberStockRepository memberStockRepository;
+    private final StockDataRepository stockDataRepository;
 
     @Transactional(readOnly = true)
     public SimulEndDto resultSimulation(Integer memberNo){
@@ -23,6 +28,28 @@ public class SimulationService {
                 memberStockRepository.findTop1ByMemberNoAndMemberStockAmountOrderByMemberStockAccEarnPriceAsc(memberNo, 0L),
                 memberStockRepository.findTop1ByMemberNoAndMemberStockAmountOrderByMemberStockAccEarnPriceDesc(memberNo, 0L)
                 );
+    }
+
+    @Transactional
+    public void updateSimulationDate(Integer memberNo, Integer simulDate){
+
+        Integer memberDate = memberAssetRepository.findById(memberNo).get().getMemberassetCurrentTime();
+        log.info("회원의 시뮬레이션 시간 : {}", memberDate);
+        String memberDateStr = null;
+
+//        while(true){
+            // 날짜 먼저 더하기
+            memberDateStr = AddDate(String.valueOf(memberDate), 0,0, simulDate);
+
+            // 해당 날짜 데이터가 있는지 확인하기
+            Integer memberDateInt = Integer.parseInt(memberDateStr);
+            StockDataInfoMapping result = stockDataRepository.findTop1ByStockDataDate(memberDateInt);
+            if(result.getStockDataDate() == null) {
+                log.info("뽑아낸 결과가 null 입니다.");
+            } else{
+            log.info("뽑아낸 결과 : {}", result.getStockDataDate());
+            }
+
     }
 
 
