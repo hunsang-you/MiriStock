8befa9a5dbdabdf8dacd40ba4ce4f0db3db16271
Anhyogwan <ahg0824@naver.com>
2023-02-06 23:01:21 +0900

    [Feat] CI/CD를 위한 actuator,Security 제외 구현
    actuator 의존성 추가, Security Webignore 추가
    application.yml 분산서버로 설정되게 작성
    actuator 에러가 나지 않게 SwaggerConfig에 webEndpointServletHandlerMapping 메서드 추가
    
    Issue:S08P12B111-535,S08P12B111-529

--- back/src/main/java/com/udteam/miristock/config/SwaggerConfig.java
+++ back/src/main/java/com/udteam/miristock/config/SwaggerConfig.java
@@ -1,17 +1,26 @@
 package com.udteam.miristock.config;
 
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.*;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 @Configuration
@@ -56,5 +65,35 @@ public class SwaggerConfig {
                     .contact(new Contact("MiriStock", "I8B111.p.ssafy.io", "MiriStock@miri.com"))
                     .version("1.0").build();
         }
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping
+            (WebEndpointsSupplier webEndpointsSupplier,
+             ServletEndpointsSupplier servletEndpointsSupplier,
+             ControllerEndpointsSupplier controllerEndpointsSupplier,
+             EndpointMediaTypes endpointMediaTypes,
+             CorsEndpointProperties corsProperties,
+             WebEndpointProperties webEndpointProperties,
+             Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(
+                webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(
+                endpointMapping, webEndpoints, endpointMediaTypes,
+                corsProperties.toCorsConfiguration(),
+                new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping, null);
+    }
 
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
+                                               Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled()
+                && (StringUtils.hasText(basePath) ||
+                ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
 }
