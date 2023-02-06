aea6cd2c9dc3056c7ca7a6250655735670c29ba5
hunsang-you <gjstkd123@naver.com>
2023-02-06 23:42:12 +0900

    [Feat] 더보기 및 관심목록

--- front/src/pages/More.js
+++ front/src/pages/More.js
@@ -3,15 +3,18 @@ import ChangeName from '../components/more/ChangeName';
 import GameExit from '../components/more/GameExit';
 import Portfolio from '../components/more/Portfolio';
 import { Routes, Route } from 'react-router-dom';
+import './css/More.css';
 
 const More = () => {
   return (
-    <Routes>
-      <Route path="" element={<MoreMain />} />;
-      <Route path="/change" element={<ChangeName />} />;
-      <Route path="/exit" element={<GameExit />} />;
-      <Route path="/result" element={<Portfolio />} />;
-    </Routes>
+    <div className="main-container">
+      <Routes>
+        <Route path="" element={<MoreMain />} />;
+        <Route path="/change" element={<ChangeName />} />;
+        <Route path="/exit" element={<GameExit />} />;
+        <Route path="/result" element={<Portfolio />} />;
+      </Routes>
+    </div>
   );
 };
 
