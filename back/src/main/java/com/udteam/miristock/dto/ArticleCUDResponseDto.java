a77cc5ccc6057a02a1c9896456b5ff35d020af07
White-Asher <dkxmpower999@naver.com>
2023-02-08 01:18:27 +0900

    [Refactor] QnA 리턴타입 객체반환, 삭제시 응답 메세지 수정
    QnA CRUD 요청완료시 요청했던 객체로 반환, 삭제응답 메세지도 반환하게 수정함
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/dto/ArticleCUDResponseDto.java
+++ back/src/main/java/com/udteam/miristock/dto/ArticleCUDResponseDto.java
@@ -4,15 +4,14 @@ import com.udteam.miristock.entity.ArticleEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Builder
 @Getter
 @ToString
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ArticleResponseDto {
+@NoArgsConstructor
+public class ArticleCUDResponseDto {
+
     private Integer articleNo; // 글 번호
     private Integer memberNo;
     private String memberNickname; // 작성자 닉네임
@@ -21,11 +20,9 @@ public class ArticleResponseDto {
     private LocalDateTime articleCreateDate; // 작성날짜
     private LocalDateTime articleModifyDate; // 수정날짜
     private Integer articleHeartCount;
-    private List<CommentResponseDto> comments;
-
 
     @Builder
-    public ArticleResponseDto(ArticleEntity entity){
+    public ArticleCUDResponseDto(ArticleEntity entity){
         this.articleNo = entity.getArticleNo();
         this.memberNo = entity.getMemberNo();
         this.memberNickname = entity.getMemberNickname();
@@ -34,9 +31,5 @@ public class ArticleResponseDto {
         this.articleCreateDate = entity.getArticleCreateDate();
         this.articleModifyDate = entity.getArticleModifyDate();
         this.articleHeartCount = entity.getArticleHeartCount();
-        this.comments = entity.getComments().stream()
-                .map(CommentResponseDto::new)
-                .collect(Collectors.toList());
     }
-
 }
