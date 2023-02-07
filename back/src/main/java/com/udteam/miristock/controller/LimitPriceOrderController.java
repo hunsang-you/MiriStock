702cee91ed40908ef51188fc6fd79a6c0788301d
White-Asher <dkxmpower999@naver.com>
2023-02-07 20:23:14 +0900

    [Feat] 거래예정목록 매도,매수타입 구분 기능 추가
    거래예정목록 매도, 매수 타입 requestparam으로 타입 지정하면 매도 예정, 매수 예정, 전체목록 출력 기능 추가
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/controller/LimitPriceOrderController.java
+++ back/src/main/java/com/udteam/miristock/controller/LimitPriceOrderController.java
@@ -3,6 +3,7 @@ package com.udteam.miristock.controller;
 import com.udteam.miristock.config.ErrorMessage;
 import com.udteam.miristock.dto.LimitPriceOrderDto;
 import com.udteam.miristock.dto.MemberDto;
+import com.udteam.miristock.entity.Deal;
 import com.udteam.miristock.service.LimitPriceOrderService;
 import com.udteam.miristock.service.MemberService;
 import com.udteam.miristock.util.HeaderUtil;
@@ -24,13 +25,13 @@ public class LimitPriceOrderController {
     private final MemberService memberService;
 
     @GetMapping
-    public ResponseEntity<List<LimitPriceOrderDto>> findAll(@RequestHeader String Authorization) {
+    public ResponseEntity<List<LimitPriceOrderDto>> findAll(@RequestHeader String Authorization, @RequestParam(required = false) Deal limitPriceOrderType) {
         MemberDto m = memberService.selectOneMember(HeaderUtil.getAccessTokenString(Authorization));
         if (m == null){
             log.info(ErrorMessage.TOKEN_EXPIRE);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else {
-            return ResponseEntity.ok().body(limitPriceOrderService.findAll(m.getMemberNo()));
+            return ResponseEntity.ok().body(limitPriceOrderService.findAll(m.getMemberNo(), limitPriceOrderType));
         }
     }
 
