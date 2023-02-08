c6eef8d2a9c5514e8231d5a8804019957214a965
White-Asher <dkxmpower999@naver.com>
2023-02-08 11:15:43 +0900

    [Refactor] 관심주식 삭제시 리턴 타입 정의, Interest 컨트롤러 분리
    관심주식 삭제시 삭제완료 -> Delete Success 메세지로 리턴 정의, interest 관련 도메인은 컨트롤러 분리
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/controller/InterestController.java
+++ back/src/main/java/com/udteam/miristock/controller/InterestController.java
@@ -1,11 +1,9 @@
 package com.udteam.miristock.controller;
 
 import com.udteam.miristock.dto.InterestDto;
-import com.udteam.miristock.dto.MemberAssetDto;
 import com.udteam.miristock.dto.MemberDto;
 import com.udteam.miristock.dto.StockDataResponseDto;
 import com.udteam.miristock.service.InterestService;
-import com.udteam.miristock.service.MemberAssetService;
 import com.udteam.miristock.service.MemberService;
 import com.udteam.miristock.util.HeaderUtil;
 import com.udteam.miristock.util.ReturnMessage;
@@ -18,47 +16,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @Slf4j
 @RestController
 @RequestMapping("/asset")
 @RequiredArgsConstructor
-public class AssetController {
+public class InterestController {
 
-    private final MemberAssetService memberAssetService;
     private final InterestService interestService;
     private final MemberService memberService;
-
-    @GetMapping
-    @ApiOperation(value = "회원 보유 자산 불러오기")
-    public ResponseEntity<MemberAssetDto> selectMemberAsset(@RequestHeader String Authorization) {
-        String token= HeaderUtil.getAccessTokenString(Authorization);
-        MemberDto m = memberService.selectOneMember(token);
-        if (m == null){
-            log.info("토큰 기간이 만료되었거나 유저 정보가 존재하지 않습니다");
-            // 엑세스 토큰 재 발급 프로세스 필요
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(null);
-        }else {
-            return ResponseEntity.ok()
-                    .body(memberAssetService.selectMemberAsset((int) m.getMemberNo()));
-        }
-    }
-
-    @GetMapping("/stockholding")
-    @ApiOperation(value = " 회원 보유 주식 목록 출력(평가금액 순)")
-    public ResponseEntity selectMemberStock(@RequestHeader String Authorization) {
-        String token= HeaderUtil.getAccessTokenString(Authorization);
-        MemberDto m = memberService.selectOneMember(token);
-//        if(m==null ){
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                    .body(null);
-//        }else {
-//            return ResponseEntity.ok()
-//                    .body(interestService.selectMemberStock(m.getMemberNo()));
-//        }
-        return null;
-    }
 
     @GetMapping("/intereststock/{stockDate}")
     @ApiOperation(value = "회원 관심 주식 목록 출력")
@@ -99,5 +64,4 @@ public class AssetController {
             return ResponseEntity.ok().body(ReturnMessage.DELETE_SUCCESS);
         }
     }
-
 }
