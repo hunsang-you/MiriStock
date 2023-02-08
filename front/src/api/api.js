3bac8675dddbf31d3b04ce4fecd9370193fc9029
배상현 <baesanghyeon@baesanghyeon-ui-MacBookAir.local>
2023-02-08 22:30:23 +0900

    [Test] Login

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_UR;
+const BASE_URL = 'https://i8b111.p.ssafy.io/api';
 const accessToken = localStorage.getItem('accessToken');
 export const api = axios.create({
   baseURL: BASE_URL,
