8fc951a551daa889a94dbe253af492af7bba6ff1
White-Asher <dkxmpower999@naver.com>
2023-02-13 22:22:46 +0900

    [Fix] 시뮬레이션 날짜 변경시 회원 보유 주식 자산 동기화 구현 및 수정
    시뮬레이션 날짜 변경시 회원 보유 주식이 적용되지 않는 문제와 부정확한 값이 대입되는 문제를 수정함
    
    Issue:none

--- back/src/main/java/com/udteam/miristock/service/MemberAssetService.java
+++ back/src/main/java/com/udteam/miristock/service/MemberAssetService.java
@@ -40,8 +40,10 @@ public class MemberAssetService {
 
 //     회원 보유 주식 자산과 날짜 업데이트하기
     public MemberAssetDto updateMemberStockAsset(Integer memberNo, Integer currentTime, String updateType) {
+        log.info("시뮬레이션 날짜가 변경되면 보유 주식의 가격변동으로 회원 주식 자산도 변동하는 메서드 호출함.");
         // 회원의 보유 주식을 들고 오기
         MemberAssetEntity memberAssetEntity = memberAssetRepository.findByMember_MemberNo(memberNo);
+        log.info("memberAssetEntity : {}", memberAssetEntity);
         List<Object[]> memberStockList = memberStockRepository.findAllMemberStockListOrderByPrice(memberNo, currentTime);
 //        List<StockDataMemberStockDto> stockDataMemberStockDtoList = new ArrayList<>();
         Long sumDiff = 0L;
@@ -49,37 +51,48 @@ public class MemberAssetService {
             MemberStockEntity memberStockEntity = (MemberStockEntity) objects[0];
             StockDataEntity stockDataEntity = (StockDataEntity) objects[1];
 //            stockDataMemberStockDtoList.add(new StockDataMemberStockDto(memberStockEntity, stockDataEntity));
-            // 회원의 한 종목 주식의 평균 매수가
-            Long memberStockAvgPrice = memberStockEntity.getMemberStockAvgPrice();
-            // 해당 주식 날짜의 종가
-            Long stockDataEntityStockDataClosingPrice = stockDataEntity.getStockDataClosingPrice();
-            Long memberStockAmount = memberStockEntity.getMemberStockAmount();
-            // 종가 - 평매가
-            Long diffAvgPrice = stockDataEntityStockDataClosingPrice - memberStockAvgPrice;
-            if(diffAvgPrice <= 0L){
-                diffAvgPrice = 0L;
-            }
-            sumDiff += diffAvgPrice;
+            log.info("memberStockEntity : {}", memberStockEntity);
+            log.info("stockDataEntity : {}", stockDataEntity);
 
+//            // 회원의 한 종목 주식의 평균 매수가
+//            Long memberStockAvgPrice = memberStockEntity.getMemberStockAvgPrice();
+//            log.info("한 종목 주식의 평균 매수가 memberStockAvgPrice :{} ", memberStockAvgPrice);
+            // 해당 주식 날짜의 종가
+//            Long stockDataEntityStockDataClosingPrice = stockDataEntity.getStockDataClosingPrice();
+//            log.info("한 종목 주식 해당날짜 종가 stockDataEntityStockDataClosingPrice : {}",stockDataEntityStockDataClosingPrice);
+            Long stockDataIncreasement = stockDataEntity.getStockDataPriceIncreasement();
+            log.info("한 종목의 등락금액 : {}", stockDataIncreasement);
+            Long memberStockAmount = memberStockEntity.getMemberStockAmount();
+            log.info("한 종목의 보유량 : {}", memberStockAmount);
+
+            // 종가 - 평매가 (차익)
+//            Long diffAvgPrice = (stockDataEntityStockDataClosingPrice - memberStockAvgPrice) * memberStockAmount;
+//            log.info("종가 - 평매가 차익 : {}",diffAvgPrice);
+            sumDiff += (stockDataIncreasement * memberStockAmount);
         }
+        log.info("sumDiff :  {}",sumDiff);
         // 멤버 전날 총 자산에 오늘 자산 데이터 업데이트하기 (전날보다 얼마나 자산이 증가했는지 계산하기 위한 데이터)
         // 당일건 거래는 이전 날짜 총 자산을 업데이트 할 필요없음
         if (updateType.equals("todayLimitPriceOrder")){
             memberAssetEntity.setMemberassetLastTotalAsset(memberAssetEntity.getMemberassetLastTotalAsset());
         }
-        // 시뮬레이션 날짜 업데이트임..
+//        // 시뮬레이션 날짜 업데이트임..
         else {
+            log.info("모든 보유 주식 자산 반영후 시뮬레이션 날짜 업데이트시 어제 날짜 반영함. memberAssetEntity.getMemberassetTotalAsset() : {}", memberAssetEntity.getMemberassetTotalAsset());
             memberAssetEntity.setMemberassetLastTotalAsset(memberAssetEntity.getMemberassetTotalAsset());
         }
         // 들고온 보유 주식과 데이터 기반으로 회원의 보유 주식 자산을 업데이트 하기
         memberAssetEntity.setMemberassetStockAsset(memberAssetEntity.getMemberassetStockAsset() + sumDiff);
+        log.info("memberAssetEntity.getMemberassetStockAsset() + sumDiff 시뮬레이션 날짜 변경후 주식 자산 : {}" ,memberAssetEntity.getMemberassetStockAsset() + sumDiff);
+        memberAssetEntity.setMemberassetAvailableAsset(memberAssetEntity.getMemberassetAvailableAsset());
         memberAssetEntity.setMemberassetTotalAsset(memberAssetEntity.getMemberassetTotalAsset() + sumDiff);
+        log.info("memberAssetEntity.getMemberassetTotalAsset() + sumDiff 시뮬레이션 날짜 변경후 총 자산 : {}", memberAssetEntity.getMemberassetTotalAsset() + sumDiff);
         memberAssetEntity.setMemberassetCurrentTime(currentTime);
+        log.info("시뮬레이션 날짜변경하면서 보유 주식 금액 반영함 : {}",currentTime);
         MemberAssetEntity result = memberAssetRepository.save(memberAssetEntity);
         log.info("회원 보유 주식 자산 업데이트됨 : {}", result);
         return MemberAssetDto.of(result);
     }
-
 
 //    public RequestSimulationDto updateMemberAssetTime(RequestSimulationDto requestSimulationDto) {
 //        return RequestSimulationDto.memberEntityToSimulationResponseDto(
